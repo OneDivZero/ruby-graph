7c546b5d131293f093562010755e4b8fa1785362
Michael Ajwani <michael.ajwani@i22.de>
2020-11-27 19:10:36 +0100

    improved test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 ENV['RAILS_ENV'] ||= 'test'
 
 INCLUDED_TEST_DIRS = %w[support].freeze
+GEM_FILE_PATH = File.dirname(__dir__)
 
 require 'ruby_graph'
 
@@ -10,5 +11,6 @@ require 'minitest/autorun'
 require 'minitest/focus'
 
 INCLUDED_TEST_DIRS.each do |folder|
-  Dir[[File.dirname(__dir__), 'test', folder, '**', '*.rb'].join('/')].sort.each { |f| require f }
+  file_list_pattern = [GEM_FILE_PATH, 'test', folder, '**', '*.rb'].join('/')
+  Dir[file_list_pattern].sort.each { |f| require f }
 end
