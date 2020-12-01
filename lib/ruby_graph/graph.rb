6b9987259987ace2cf241161fcb80d7723063dac
Michael Ajwani <michael.ajwani@i22.de>
2020-12-01 20:11:30 +0100

    coded basic graph-operations | Issue #1

--- lib/ruby_graph/graph.rb
+++ lib/ruby_graph/graph.rb
@@ -10,5 +10,58 @@ module RubyGraph
       @name = name || object_id
       @storage = {}
     end
+
+    def empty?
+      @storage.empty?
+    end
+
+    # Responds if a node is known
+    def node?(name)
+      @storage.key?(node_key_for(name))
+    end
+
+    # Returns the node-definition
+    # Currently the same as :neighbors
+    def node(name)
+      @storage[node_key_for(name)]
+    end
+
+    # Returns the neighbors of a node
+    def neighbors(name)
+      @storage[node_key_for(name)]
+    end
+
+    # Adds a new node to graph, unless it already exists
+    # Connects it to another node, if a target named :to is present
+    # DOCME: integers with a leading zero like phone-numbers will always be converted by ruby, e.g. 0221 => 145
+    def add(name, to: nil)
+      add_node(name) unless node?(name)
+
+      return true unless to.present?
+      return false unless node?(to)
+
+      add(to)
+      connect(name, to)
+    end
+
+    def connect(source, target)
+      source = node_key_for(source)
+      target = node_key_for(target)
+
+      return false unless node?(source)
+      return false unless node?(target)
+
+      @storage[source] << target
+      @storage[target] << source unless source.eql?(target)
+      true
+    end
+
+    private def add_node(name)
+      @storage[node_key_for(name)] = []
+    end
+
+    private def node_key_for(name)
+      name.to_s.to_sym
+    end
   end
 end
--- test/ruby_graph/graph_test.rb
+++ test/ruby_graph/graph_test.rb
@@ -1,18 +1,110 @@
 require 'test_helper'
 
 # NOTE: We use :active_support instead of using plain minitest (=> 'class RubyGraph::GraphTest < Minitest::Test')
+# Just because it provides more painless testing-capabilities. (MiniTest needs to become a bit more powerfull!)
+
 class RubyGraph::GraphTest < ActiveSupport::TestCase
+  private def build_graph(name: nil)
+    @graph = RubyGraph::Graph.build(name: name)
+  end
+
   test 'it has a name by default' do
-    g = RubyGraph::Graph.build
+    build_graph
 
-    assert_not_nil g.name
-    assert_equal g.object_id, g.name
+    assert_not_nil @graph.name
+    assert_equal @graph.object_id, @graph.name
   end
 
-  test 'it could have a custom name' do
-    g = RubyGraph::Graph.build(name: 'custom')
+  test 'it accepts a custom name' do
+    build_graph(name: :custom)
 
-    assert_not_nil g.name
-    assert_equal 'custom', g.name
+    assert_not_nil @graph.name
+    assert_equal :custom, @graph.name
   end
+
+  test 'it is an empty graph by default' do
+    build_graph
+
+    assert @graph.empty?
+  end
+
+  test 'it adds a node and accepts a symbol or string or number as name' do
+    build_graph
+
+    [:a, 'b', 123].each do |name|
+      @graph.add(name)
+
+      assert @graph.storage.key?(name.to_s.to_sym)
+    end
+  end
+
+  test 'it adds a node and connects it with an existing node' do
+    build_graph
+
+    assert_not @graph.add(:a, to: :b)
+
+    @graph.add(:b)
+
+    assert @graph.add(:a, to: :b)
+  end
+
+  test 'responds if a node is known' do
+    build_graph
+    @graph.add(:a)
+
+    assert @graph.node?(:a)
+    assert_not @graph.node?(:unknown)
+  end
+
+  test 'it returns a node-definition if the node is known' do
+    build_graph
+    @graph.add(:a)
+
+    assert @graph.node(:a)
+    assert_equal [], @graph.node(:a)
+    assert_nil @graph.node(:unknown)
+  end
+
+  test 'it can connect two nodes' do
+    build_graph
+    @graph.add(:a)
+    @graph.add(:b)
+
+    assert @graph.connect(:a, :b)
+    assert_equal [:b], @graph.storage[:a]
+    assert_equal [:a], @graph.storage[:b]
+  end
+
+  test 'it can connect a node with itself' do
+    build_graph
+    @graph.add(:a)
+
+    assert @graph.connect(:a, :a)
+    assert_equal [:a], @graph.storage[:a]
+  end
+
+  test 'neighbors' do
+    build_graph
+    @graph.add(:a)
+    @graph.add(:b)
+    @graph.add(:c)
+
+    assert_empty [], @graph.neighbors(:a)
+
+    @graph.connect(:a, :b)
+
+    assert_equal [:b], @graph.neighbors(:a)
+
+    @graph.connect(:a, :c)
+
+    assert_equal [:b, :c], @graph.neighbors(:a)
+    assert_equal [:a], @graph.neighbors(:b)
+    assert_equal [:a], @graph.neighbors(:c)
+  end
+
+  # describe 'Nodes' do
+  #   test 'responds if a node is unknown' do
+  #     assert true
+  #   end
+  # end
 end
